bea919acf462ddd898b407ea749e35e0368a0708
Aexer <302801376@student.rocmondriaan.nl>
2022-05-24 18:35:11 +0200

    initial commit

--- modules/navbar.php
+++ modules/navbar.php
@@ -1,16 +1,16 @@
 <nav class="navbar">
-  <ul class="nav-list w-100">
+  <ul class="nav-list w-100 m-0 p-0 d-flex list-unstyled">
     <li class="nav-item">
-      <a href="#" class="nav-link active">Home</a>
+      <a href="index.php" class="nav-link active">Home</a>
     </li>
     <li class="nav-item">
-      <a href="#" class="nav-link">Categories</a>
+      <a href="categorieen.php" class="nav-link">Categorieen</a>
     </li>
     <li class="nav-item">
-      <a href="#" class="nav-link">About</a>
+      <a href="contact.php" class="nav-link">Contact</a>
     </li>
     <li class="nav-item ms-auto">
-      <a href="#" class="nav-link">Registreren</a>
+      <a href="user.php" class="nav-link">Registreren</a>
     </li>
     <li class="nav-item">
       <a href="#" class="nav-link">Inloggen</a>
