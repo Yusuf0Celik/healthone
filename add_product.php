b3a24654855862f5330d8c471d6d960d27c14381
Aexer <302801376@student.rocmondriaan.nl>
2022-06-16 21:08:56 +0200

    file unlinked after edit product. head is now in components. profile is done

--- add_product.php
+++ add_product.php
@@ -62,75 +62,64 @@ if (!isset($userRole) == 'admin') {
 ?>
   <!DOCTYPE html>
   <html lang="en">
-
-  <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossorigin="anonymous"></script>
-    <link rel="stylesheet" href="css/style.css">
-    <title>Healthone</title>
-  </head>
-
-  <body>
-    <div class="container p-3 my-4">
-      <?php
-      include_once 'components/header.php';
-      include_once 'components/navbar.php';
-      include_once 'components/picture.php';
-      
-      if(isset($statusAlert)) 
-      { 
-        echo "<div class='alert $statusAlert' role='alert'>$statusMessage</div>";
-      } else {
-        echo '';
-      }
-      ?>
-      <form action="" method="post" enctype='multipart/form-data'>
-      <div class="row my-4">
-        <div class="col">
-          <label for="name">Product Naam:</label>
-          <input type="text" name="productName" class="form-control" placeholder="Product Naam">
-        </div>
-        <div class="col">
-          <label for="category">Category:</label>
-          <select class="form-select" name="categorySelect">
-            <?php
-            foreach ($result as &$data) {
-            ?>
-              <option value="<?php echo $data["id"] ?>" <?php if ($data["id"] == 1) {echo "selected";} ?>><?php echo $data["name"] ?></option>
-            <?php
-            }
-            ?>
-          </select>
-        </div>
+  <?php  
+  include_once 'components/head.php';
+  ?>
+<body>
+  <div class="container p-3 my-4">
+    <?php
+    include_once 'components/header.php';
+    include_once 'components/navbar.php';
+    include_once 'components/picture.php';
+    
+    if(isset($statusAlert)) 
+    { 
+      echo "<div class='alert $statusAlert' role='alert'>$statusMessage</div>";
+    } else {
+      echo '';
+    }
+    ?>
+    <form action="" method="post" enctype='multipart/form-data'>
+    <div class="row my-4">
+      <div class="col">
+        <label for="name">Product Naam:</label>
+        <input type="text" name="productName" class="form-control" placeholder="Product Naam">
       </div>
-      <div class="row mb-4">
-        <div class="col">
-          <label for="detail">Beschrijving:</label>
-          <textarea type="text" name="productDetail" class="form-control"></textarea>
-        </div>
+      <div class="col">
+        <label for="category">Category:</label>
+        <select class="form-select" name="categorySelect">
+          <?php
+          foreach ($result as &$data) {
+          ?>
+            <option value="<?php echo $data["id"] ?>" <?php if ($data["id"] == 1) {echo "selected";} ?>><?php echo $data["name"] ?></option>
+          <?php
+          }
+          ?>
+        </select>
       </div>
-      <div class="row mb-4">
-        <div class="col">
-          <label for="detail">Image bij sportapparaat:</label>
-          <div class="input-group mb-3">
-            <input type="file" name="file_upload" class="form-control">
-          </div>
-        </div>
-      </div>
-        <button type="submit" class="btn btn-succes" name="submit">Add Product</button>
-      </form>
-      <?php
-      
-      include_once 'components/footer.php';
-      ?>
     </div>
-  </body>
+    <div class="row mb-4">
+      <div class="col">
+        <label for="detail">Beschrijving:</label>
+        <textarea type="text" name="productDetail" class="form-control"></textarea>
+      </div>
+    </div>
+    <div class="row mb-4">
+      <div class="col">
+        <label for="detail">Image bij sportapparaat:</label>
+        <div class="input-group mb-3">
+          <input type="file" name="file_upload" class="form-control">
+        </div>
+      </div>
+    </div>
+      <button type="submit" class="btn btn-succes" name="submit">Add Product</button>
+    </form>
+    <?php
+    
+    include_once 'components/footer.php';
+    ?>
+  </div>
+</body>
 
   </html>
 <?php
--- profile.php
+++ profile.php
@@ -4,35 +4,97 @@ include 'logged_in_user.php';
 if (!isset($_SESSION["loggedin_user_id"])) {
   echo "Error: Log in first!";
 } else {
-  ?>
-<!DOCTYPE html>
-<html lang="en">
-  <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-  <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossorigin="anonymous"></script>
-  <link rel="stylesheet" href="css/style.css">
-  <title>Healthone</title>
-</head>
-<body>
-  <div class="container p-3 my-4">
-  <?php
-  include_once 'components/header.php';
-  include_once 'components/navbar.php';
-  include_once 'components/picture.php';
-  
-  ?>
-  
-  <?php
-  include_once 'components/footer.php';
+
+  if (isset($_POST["submit"])) {
+    $password = $_POST["password"];
+    $confirmPassword = $_POST["confirm_password"];
+    $nameUser = $_POST["username"];
+    $emailUser = $_POST["email"];
+
+    if ($password != $confirmPassword) {
+      $statusAlert = 'alert-danger';
+      $statusMessage = 'Wachtwoord komt niet overeen';
+    } else {
+      if ($password == '' || $confirmPassword == '') {
+        $statusAlert = 'alert-danger';
+        $statusMessage = 'Vul alle velden in';
+        $products = $db->prepare("UPDATE users SET name = :name, email = :email WHERE id = :id");
+        $products->bindParam("name", $nameUser);
+        $products->bindParam("email", $emailUser);
+        $products->bindParam("id", $_SESSION['loggedin_user_id']);
+        if ($products->execute()) {
+          $statusAlert = 'alert-succes';
+          $statusMessage = 'Profiel geupdate!';
+        }
+      } else {
+        $products = $db->prepare("UPDATE users SET name = :name, email = :email, password = :password WHERE id = :id");
+        $products->bindParam("name", $nameUser);
+        $products->bindParam("email", $emailUser);
+        $products->bindParam("password", $password);
+        $products->bindParam("id", $_SESSION['loggedin_user_id']);
+        if ($products->execute()) {
+          $statusAlert = 'alert-succes';
+          $statusMessage = 'Wachtwoord geupdate!';
+        } else {
+          $statusAlert = 'alert-danger';
+          $statusMessage = 'Er is iets mis gegaan';
+        }
+      }
+    }
+  }
 ?>
-</div>
-</body>
-</html>
+  <!DOCTYPE html>
+  <html lang="en">
+  <?php
+  include_once 'components/head.php';
+  ?>
+
+  <body>
+    <div class="container p-3 my-4">
+      <?php
+      include_once 'components/header.php';
+      include_once 'components/navbar.php';
+      include_once 'components/picture.php';
+      if (isset($statusAlert)) {
+        echo "<div class='alert $statusAlert' role='alert'>$statusMessage</div>";
+      } else {
+        echo '';
+      }
+      ?>
+      <div class="col-sm-12 mt-4">
+        <h4>Jouw profiel</h4>
+        <form method="POST" action="">
+          <input type="hidden" name="form-sort" value="editprofile">
+          <div class="row mt-3">
+            <div class="col">
+              <input type="text" name="username" class="form-control" value="<?php echo $userName ?>" placeholder="Uw naam" required>
+            </div>
+            <div class="col">
+              <input type="email" name="email" class="form-control" value="<?php echo $userEmail ?>" placeholder="jan@jan.nl" required>
+            </div>
+          </div>
+          <div class="row mt-3">
+            <div class="col">
+              <label for="password">Voer een nieuw wachtwoord in</label>
+              <input type="password" name="password" class="form-control">
+              <small class="text-danger">*Laat het wachtwoord leeg om het niet aan te passen!</small>
+            </div>
+            <div class="col">
+              <label for="confirm_password">Herhaal je wachtwoord</label>
+              <input type="password" name="confirm_password" class="form-control">
+            </div>
+          </div>
+          <div class="form-group mt-3">
+            <button type="submit" name="submit" class="btn btn-outline-danger">Profiel aanpassen</button>
+          </div>
+        </form>
+      </div>
+      <?php
+      include_once 'components/footer.php';
+      ?>
+    </div>
+  </body>
+
+  </html>
 <?php
 }
